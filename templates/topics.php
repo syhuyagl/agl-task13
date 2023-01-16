707eb386c6301744017166d19aee02b0ce0aae1e
AGL_Hồ Sỹ Huy  (フイ) <ho_sy_huy@allgrow-labo.jp>
2023-01-16 11:35:30 +0700

    update

--- templates/topics.php
+++ templates/topics.php
@@ -8,41 +8,46 @@ Template Name: Topics
     <div class="c-title c-title--page">
         <h1>TOPICS</h1>
     </div>
+
     <div class="l-container">
         <ul class="c-listpost">
             <?php
-            $topics = get_posts(array('orderby' => 'date', 'order' => 'DESC', 'numberposts' => 10, ));
-            foreach ($topics as $topic):
-                setup_postdata($topic);
-                ?>
-                <li>
-                    <span class="datepost"><?php echo get_the_date('Y/m/d', $topic); ?></span>
-                    <span class="c-labellist">
-                        <?php
-                        $cats = get_the_category($topic); foreach ($cats as $cat) {
-                            if ($cat->cat_name != 'サービス') {
-                                ?>
-                                <a href="<?php echo get_category_link($cat->term_id); ?>" class="c-label">
-                                    <?php echo $cat->cat_name; ?>
-                                </a>
-                            <?php }
-                        } ?>
-                    </span>
-                    <a href="<?php echo get_permalink($topic->ID); ?>">
-                        <?php echo get_the_title($topic->ID); ?>
-                    </a>
-                </li>
-            <?php endforeach; ?>
+            $postOffset = $paged * 5;
+            $get_post_query = array(
+                'post_type' => 'post',
+                'orderby' => 'date',
+                'order' => 'DESC',
+                'posts_per_page' => 10,
+                'paged' => (get_query_var('paged') ? get_query_var('paged') : 1),
+            );
+            $topics = new WP_Query($get_post_query);
+            if ($topics->have_posts()):
+                while ($topics->have_posts()):
+                    $topics->the_post();
+                    ?>
+                    <li>
+                        <span class="datepost"><?php echo get_the_date('Y/m/d', $topic); ?></span>
+                        <span class="c-labellist">
+                            <?php
+                            $cats = get_the_category($topic);
+                            foreach ($cats as $cat) {
+                                if ($cat->cat_name != 'サービス') {
+                                    ?>
+                                    <a href="<?php echo get_category_link($cat->term_id); ?>" class="c-label">
+                                        <?php echo $cat->cat_name; ?>
+                                    </a>
+                                <?php }
+                            } ?>
+                        </span>
+                        <a href="<?php echo get_permalink($topic->ID); ?>">
+                            <?php echo get_the_title($topic->ID); ?>
+                        </a>
+                    </li>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </ul>
-        </ul>
-        <div class="c-pnav">
-            <?php the_posts_pagination(
-                array(
-                    'mid_size' => 4,
-                    'prev_text' => __('', 'textdomain'),
-                    'next_text' => __('', 'textdomain'),
-                )
-            ); ?>
+        <div class="c-topicnav">
+            <?php custom_pagination('', 2, $topics); ?>
         </div>
     </div>
 </main>
